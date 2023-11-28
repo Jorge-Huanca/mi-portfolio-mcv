18eb6cb88810fd990fbbc3380c3f384c67fc603f
Jorge Huanca <jorgeh_98_jc@hotmail.com>
2023-11-28 00:15:42 -0300

    entrega portflio

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const homeRouter = require("./routers/main.js")
+const aboutRouter = require("./routers/about.js")
+
+app.use(express.static("public"));
+
+app.use("/about", aboutRouter);
+app.use("/", homeRouter);
+
+app.listen(3000,() =>{
+    console.log("Servidor en el puerto 3000");
+})
+
